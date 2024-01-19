756a4c7ae45bf31bb095df4fca6e9fafc1f89c1b
Declan Caulfield <decant09@hotmail.com>
2024-01-19 19:08:17 +0000

    Complete webhook handler & payment system

--- checkout/models.py
+++ checkout/models.py
@@ -7,7 +7,6 @@ from django.conf import settings
 from products.models import Product
 
 
-
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
@@ -28,6 +27,10 @@ class Order(models.Model):
         )
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0
+        )
+    original_cart = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(
+        max_length=254, null=False, blank=False, default=''
         )
 
     def _generate_order_number(self):
