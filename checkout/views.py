b498cf0089a55b2fbdf3ed2462c9fe263b980d0d
Declan Caulfield <decant09@hotmail.com>
2024-01-20 12:57:33 +0000

    Prepopulate profile info in form on checkout

--- checkout/views.py
+++ checkout/views.py
@@ -110,9 +110,27 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        print(intent)
-
-        order_form = OrderForm()
+        """Attempt to prefill the form with any info the user
+        maintains in their profile"""
+        
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'town_or_city': profile.default_town_or_city,
+                    'county': profile.default_county,
+                    'postcode': profile.default_postcode,
+                    'country': profile.default_country,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
