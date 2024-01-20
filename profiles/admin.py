855822b3c7cfbadcb82b672c6bc9f41e1cc92aa3
Declan Caulfield <decant09@hotmail.com>
2024-01-20 15:53:34 +0000

    Add UserProfile to admin panel

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    """
+    UserProfile Model Admin
+    """
+
+    fields = (
+        'user', 'default_street_address1', 'default_street_address2',
+        'default_town_or_city', 'default_county', 'default_postcode',
+        'default_country', 'default_phone_number',)
+
+    list_display = (
+        'user', 'default_town_or_city',
+        'default_country', 'default_phone_number',)
+
+    ordering = ('user',)
+    
+
+admin.site.register(UserProfile, UserProfileAdmin)
