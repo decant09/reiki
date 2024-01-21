62e84304b337e7705f1bf853f216428a61b9724b
Declan Caulfield <decant09@hotmail.com>
2024-01-21 21:23:04 +0000

    Add homepage reviews

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from review.models import Review
 
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    reviews = Review.objects.filter(homepage=True)
+    context = {
+        'homepage_reviews': reviews,
+    }
+
+    return render(request, 'home/index.html', context)
