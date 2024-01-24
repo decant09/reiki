32f6e4250ccba64daa9e26362098d3a016406c50
Declan Caulfield <decant09@hotmail.com>
2024-01-24 12:31:56 +0000

    Add PEP8 amendments

--- faq/admin.py
+++ faq/admin.py
@@ -3,8 +3,7 @@ from .models import Faq
 
 
 class FaqAdmin(admin.ModelAdmin):
-    list_display = ('question', 'answer',
-    )
+    list_display = ('question', 'answer',)
 
 
 admin.site.register(Faq, FaqAdmin)
